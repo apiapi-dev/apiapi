638c837194b8d6e48dbfb6cccc45e93a9373b052
Martha <mm@marthas-air.lan>
2021-01-20 15:04:18 -0800

    finished the creator section in the about page

--- src/components/about/Card.tsx
+++ src/components/about/Card.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import linkedin from '../../assets/images/linkedin.png';
-
+import GitHub from '../../assets/images/github-card.png';
 type CardProps = {
   name: string;
   role: string;
-  url: string;
+  liurl: string;
+  giurl: string;
 };
 
-export default function Card({ name, role, url }: CardProps) {
+export default function Card({ name, role, liurl, giurl }: CardProps) {
   return (
     <div className="name-card">
-      <h3>{name}</h3>
+      <h2>{name}</h2>
       <p>{role}</p>
-      <a href={url}>
+      <a href={liurl}>
         <img src={linkedin}></img>
+      </a>
+      <a href={giurl}>
+        <img src={GitHub}></img>
       </a>
     </div>
   );
--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -4,23 +4,27 @@ import Card from './Card';
 export default function index() {
   return (
     <div className="about">
-      <h2></h2>
-      <h2>Meet the creaters</h2>
-      <div className="creaters">
+      <h2>About apiapi</h2>
+      <p>.....</p>
+      <h2>Meet the creators</h2>
+      <div className="creators">
         <Card
           name="Benjamin Swerdlow"
           role="Lead Backend developer"
-          url="https://www.linkedin.com/in/benswerdlow/"
+          liurl="https://www.linkedin.com/in/benswerdlow/"
+          giurl="https://github.com/theswerd"
         ></Card>
         <Card
-          name="Josh Bubar"
+          name="Joshua Bubar"
           role="Frontend developer"
-          url="https://www.linkedin.com/in/jbubar/"
+          liurl="https://www.linkedin.com/in/jbubar/"
+          giurl="https://github.com/jbubar"
         ></Card>
         <Card
           name="Martha Martinez"
           role="Frontend developer"
-          url="https://www.linkedin.com/in/mmartham/"
+          liurl="https://www.linkedin.com/in/mmartham/"
+          giurl="https://github.com/martham0"
         ></Card>
       </div>
     </div>
