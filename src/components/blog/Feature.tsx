ef7687dda201473fa69a8fbfeac05e53549c1556
Martha <mm@marthas-air.lan>
2021-01-20 16:14:20 -0800

    css fixes

---
+++ src/components/blog/Feature.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Feature() {
+  return (
+    <div>
+      <h1>blog</h1>
+    </div>
+  );
+}
--- src/components/blog/index.tsx
+++ src/components/blog/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Feature from './Feature';
 
 export default function index() {
   return (
     <div>
       <h1>blog</h1>
+      <Feature></Feature>
+      <Feature></Feature>
+      <Feature></Feature>
+      <Feature></Feature>
     </div>
   );
 }
